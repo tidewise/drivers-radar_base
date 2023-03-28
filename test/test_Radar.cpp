15090f733ebef00e717492843955c4549383a637
Afonso da Fonseca Braga <afonso.braga@tidewise.io>
2023-03-28 14:37:49 -0300

    chore: add content test

--- test/test_Radar.cpp
+++ test/test_Radar.cpp
@@ -20,10 +20,18 @@ TEST_F(RadarTest, it_creates_an_object_correctly)
     radar.addEcho(range, sweep_length, step_size, heading_0, echo);
     radar.addEcho(range, sweep_length, step_size, heading_1, echo);
     radar.addEcho(range, sweep_length, step_size, heading_2, echo);
+    uint8_t echo_result[3 * sizeof(echo)];
+    memcpy(echo_result, echo, sizeof(echo));
+    memcpy(echo_result + sizeof(echo), echo, sizeof(echo));
+    memcpy(echo_result + 2 * sizeof(echo), echo, sizeof(echo));
+
     ASSERT_NO_THROW();
     ASSERT_EQ(radar.sweep_timestamps.size(), 3);
-    ASSERT_EQ(radar.sweep_data.size(), 30);
     ASSERT_EQ(radar.step_angle.rad, step_size.rad);
+    ASSERT_EQ(radar.sweep_data.size(), sizeof(echo_result));
+    for (int i = 0; i < static_cast<int>(sizeof(echo_result)); i++) {
+        ASSERT_EQ(radar.sweep_data.at(i), echo_result[i]);
+    }
 }
 
 TEST_F(RadarTest, it_crashes_with_different_step_size)
