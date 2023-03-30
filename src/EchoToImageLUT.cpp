0779b0a583979125873fdb3b9ccc93d81d415e9b
Afonso da Fonseca Braga <afonso.braga@tidewise.io>
2023-03-30 10:55:08 -0300

    feat: add LookUpTable Helper

---
+++ src/EchoToImageLUT.cpp
@@ -0,0 +1,121 @@
+#include "EchoToImageLUT.hpp"
+#include <algorithm>
+#include <iostream>
+
+using namespace radar_base;
+using namespace cv;
+using namespace std;
+
+int discretizeAngle(double theta_rad, int num_angles)
+{
+    double angle_step = 2 * M_PI / num_angles;
+    return round(theta_rad / angle_step);
+}
+
+EchoToImageLUT::EchoToImageLUT(int num_angles,
+    int sweep_size,
+    float beam_width,
+    int window_size)
+    : m_sweep_size(sweep_size)
+{
+    setup(sweep_size, window_size, num_angles, beam_width);
+}
+
+void EchoToImageLUT::setup(int sweep_size,
+    int window_size,
+    int num_angles,
+    float beam_width)
+{
+    auto raw_data = computeRawLUTTable(sweep_size, window_size, num_angles, beam_width);
+    linearizeRawTable(raw_data);
+    m_sweep_size = sweep_size;
+}
+
+vector<vector<Point>> EchoToImageLUT::computeRawLUTTable(int sweep_size,
+    int window_size,
+    int num_angles,
+    float beam_width)
+{
+    RawTable data;
+    data.resize(num_angles * sweep_size);
+
+    float const pixel_per_cell = (window_size / 2.0) / sweep_size;
+    float const half_beam_width = beam_width / 2;
+
+    Point img_center(window_size / 2, window_size / 2);
+    for (auto i = 0; i < window_size; i++) {
+        for (auto j = 0; j < window_size; j++) {
+            Point2i current_p(j, i);
+            Point2i relative_p = img_center - current_p;
+            double distance = norm(relative_p);
+            int echo_index = round(distance / pixel_per_cell);
+            if (echo_index >= sweep_size) {
+                continue;
+            }
+
+            double theta_rad = atan2(relative_p.x, relative_p.y);
+            int theta0 = discretizeAngle(theta_rad - half_beam_width, num_angles);
+            int theta1 = discretizeAngle(theta_rad + half_beam_width, num_angles);
+            for (int t = theta0; t <= theta1; ++t) {
+                addRawLUTEntry(data, t, echo_index, sweep_size, num_angles, current_p);
+            }
+        }
+    }
+    return data;
+}
+
+void EchoToImageLUT::linearizeRawTable(RawTable const& table)
+{
+    m_data_index.resize(table.size() + 1);
+    m_data.clear();
+    for (size_t global_echo_id = 0; global_echo_id < table.size(); global_echo_id++) {
+        m_data_index[global_echo_id] = m_data.size();
+        for (size_t point_id = 0; point_id < table[global_echo_id].size(); point_id++) {
+            m_data.push_back(table[global_echo_id][point_id]);
+        }
+    }
+    m_data_index[table.size()] = m_data.size();
+}
+
+int EchoToImageLUT::normalizeAngle(int angle, int num_angles)
+{
+    angle = angle % num_angles;
+    if (angle < 0) {
+        return angle + num_angles;
+    }
+    return angle;
+}
+
+void EchoToImageLUT::addRawLUTEntry(RawTable& table,
+    int angle,
+    int echo_index,
+    int sweep_size,
+    int num_angles,
+    Point2i const& p)
+{
+    angle = normalizeAngle(angle, num_angles);
+    table[angle * sweep_size + echo_index].push_back(p);
+}
+
+void EchoToImageLUT::updateImage(Mat& image, int angle, int echo_index, int echo) const
+{
+    if (echo < 0) {
+        echo = 0;
+    }
+    int data_i = angle * m_sweep_size + echo_index;
+    auto begin = m_data_index[data_i];
+    auto end = m_data_index[data_i + 1];
+    for (auto id = begin; id < end; id++) {
+        auto p = m_data[id];
+
+        auto& current = image.at<Vec3b>(p);
+        auto v = std::max<int>(current[0], echo);
+        current = Vec3b(v, v, v);
+    }
+}
+
+Point EchoToImageLUT::fetchEntry(int angle, int echo_index_in_sweep) const
+{
+    auto data_i = angle * m_sweep_size + echo_index_in_sweep;
+    return m_data[m_data_index[data_i]];
+}
---
+++ src/EchoToImageLUT.hpp
@@ -0,0 +1,59 @@
+#ifndef RADAR_BASE_ECHO_TO_IMAGE_LUT_HPP
+#define RADAR_BASE_ECHO_TO_IMAGE_LUT_HPP
+
+#include <opencv2/core.hpp>
+#include <vector>
+
+namespace radar_base {
+    class EchoToImageLUT {
+        /** m_data[angle * sweepSize + cellIndex] contains the list of points in the image
+         * that should be filled by the echo with the given angle and cell
+         */
+        std::vector<cv::Point> m_data;
+        std::vector<int> m_data_index;
+
+        int m_sweep_size;
+
+        static int normalizeAngle(int angle, int num_angles);
+
+        typedef std::vector<std::vector<cv::Point>> RawTable;
+        static RawTable computeRawLUTTable(int sweep_size,
+            int window_size,
+            int num_angles,
+            float beam_width);
+        static void addRawLUTEntry(RawTable& table,
+            int angle,
+            int echo_index,
+            int sweep_size,
+            int num_angles,
+            cv::Point2i const& p);
+
+        void setup(int sweep_size, int window_size, int num_angles, float beam_width);
+
+    public:
+        EchoToImageLUT() = delete;
+
+        /**
+         * Constructor that creates a LUT matrix based on radar echoes.
+         * @brief Constructor.
+         * @param num_angles Amount of samples contained in a radar revolution.
+         * @param sweep_size Number of measurements inside a single sweep.
+         * @param beam_width Angle between the 3dB half power points on the main lobe.
+         * @param window_size The param will create a squared frame.
+         */
+        EchoToImageLUT(int num_angles, int sweep_size, float beam_width, int window_size);
+        ~EchoToImageLUT() = default;
+
+        void updateImage(cv::Mat& image, int angle, int echo_index, int echo) const;
+
+        void updateAngle(cv::Mat& image, int angle, int echo_index, int echo) const;
+
+        void clearBeamEndAtImage(cv::Mat& image, int angle, int echo_index) const;
+
+        void linearizeRawTable(RawTable const& table);
+
+        cv::Point fetchEntry(int angle, int echo_index) const;
+    };
+}
+
+#endif
